cd81a21cdfa7d4e187a94840d8658f208f03c28e
John Charles Frederick S. Mamanao <79141178+Beast11432@users.noreply.github.com>
2021-12-27 15:42:44 +0800

    Update script.js

--- TheVikings/script.js
+++ TheVikings/script.js
@@ -74,3 +74,15 @@ function validate(){
 
   }
 }
+
+const menuTabs = document.querySelector(".menu-tabs");
+menuTabs.addEventListener("click",function(e){
+    if(e.target.classList.contains("menu-tab-item") && !e.target.classList.contains("active")){
+        const target = e.target.getAttribute("data-target");
+        menuTabs.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const menuSection = document.querySelector(".menu-section");
+        menuSection.querySelector(".menu-tab-content.active").classList.remove("active");
+        menuSection.querySelector(target).classList.add("active");
+    }
+})
